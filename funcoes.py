a9864a2f132fb68489646edc9d780e3ab7ee54c2
Beatriz Borges <zackiewiczbeatriz@gmail.com>
2022-04-29 11:45:22 -0300

    def normalizando a base de paises 1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,14 @@
 # FUNÇÕES BASE DO EP2
+
+def normaliza_bp(DADOS):
+    bpaises = {}
+
+    for continente in DADOS:
+        pcont = DADOS[continente]
+        for pais in pcont:
+            infop = pcont[pais]
+            infop['continente'] = continente
+
+            bpaises[pais] = infop 
+
+    return bpaises 
