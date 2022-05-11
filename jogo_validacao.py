2b38459980afa49e2588ce65042d9f7c4c39716e
Beatriz Borges <zackiewiczbeatriz@gmail.com>
2022-05-11 14:57:47 -0300

    arrumado

--- jogo_validacao.py
+++ jogo_validacao.py
@@ -47,7 +47,7 @@ while palpite != pais:
         l2 = infopalpite["geo"]["latitude"]
         # distancia de pais --> palpite
         distancia = haversine(EARTH_RADIUS, p1,l1,p2,l2) # tirar casas decimais
-        mostradist += '\n {} --> {} \n'.format(distancia, palpite) 
+        mostradist += '\n {} --> {}'.format(distancia, palpite) 
         print(mostradist)
         print(mostradica) 
 
